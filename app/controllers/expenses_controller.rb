d3e64d26b0f2b528a6a545b147e4d1e098884c30
YYYukiyamazakIII <fairy_0530_0710@yahoo.co.jp>
2022-06-08 23:23:33 +0900

    expense/index.hrml.erbの修正

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,18 +1,8 @@
 class ExpensesController < ApplicationController
   def index
-    @current_user_expense = Expense.where(user_id: current_user.id)
-    if params[:start_date].nil?
-      today = Time.now
-      today = today.to_s
-      @expenses = @current_user_expense.where('date like ?', "#{today.slice(0..6)}%")
-    else
-      @expenses = @current_user_expense.where('date like ?', "#{params[:start_date].slice(0..6)}%")
-    end
-    @total_value = 0
-    @expenses.each do |expense|
-      @total_value += expense.value
-    end
-    @category_count = Category.count
+    set_expenses
+    set_total_expense
+    @category = Category.all
   end
 
   def new
@@ -61,7 +51,25 @@ class ExpensesController < ApplicationController
 
   private
 
+  def set_expenses
+    @current_user_expense = Expense.where(user_id: current_user.id)
+    if params[:start_date].nil?
+      today = Time.now.to_s
+      @expenses = @current_user_expense.where('date like ?', "#{today.slice(0..6)}%")
+    else
+      @expenses = @current_user_expense.where('date like ?', "#{params[:start_date].slice(0..6)}%")
+    end
+  end
+
+  def set_total_expense
+    @total_expense = 0
+    @expenses.each do |expense|
+      @total_expense += expense.value
+    end
+  end
+
   def expense_params
     params.require(:expense).permit(:name, :value, :category_id, :date).merge(user_id: current_user.id)
   end
+
 end
