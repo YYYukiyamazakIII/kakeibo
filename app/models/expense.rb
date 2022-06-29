40aa5999c55446d1e939376c03e5e3dd459c0fe0
YYYukiyamazakIII <fairy_0530_0710@yahoo.co.jp>
2022-06-29 22:56:44 +0900

    expenseのバリテーション記述

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,6 +1,8 @@
 class Expense < ApplicationRecord
-  validates :date, :name, :value, :category_id, :user_id, presence: true
-  validates :category_id, numericality: { other_than: 1, message: 'を入力してください' }
+  validates :date, :user_id, presence: true
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :value, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 9999999 }
+  validates :category_id, presence: true, numericality: { other_than: 1, message: 'を入力してください' }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
