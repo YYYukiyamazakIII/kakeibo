17b77f08765103987da4879a125579c461a4f048
YYYukiyamazakIII <fairy_0530_0710@yahoo.co.jp>
2022-07-12 23:48:15 +0900

    userモデルの正常系テスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー式登録' do
+
+    it 'name,email,password,password_confirmation,prefecture_id,city,profileが存在すれば登録できる' do
+      expect(@user).to be_valid
+    end
+    
+  end
 end
