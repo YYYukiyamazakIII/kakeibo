9d542443a522d5b96dc28bbdae1968b892bbb529
YYYukiyamazakIII <fairy_0530_0710@yahoo.co.jp>
2022-07-25 16:53:45 +0900

    'ログインができる時'の実装

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -36,7 +36,7 @@ RSpec.describe "ユーザー新規登録", type: :system do
       expect(page).to have_no_content('ログイン')
     end
   end
- 
+
   context 'ユーザー新規登録ができない時' do
     it '誤った情報ではユーザー新規登録ができずに新規登録ページに戻ってくる' do
       # トップページに移動する
@@ -58,6 +58,39 @@ RSpec.describe "ユーザー新規登録", type: :system do
       }.to change { User.count }.by(0)
       # 新規登録ページへ戻されることを確認する
       expect(current_path).to eq user_registration_path
+    end
+  end
+end
+
+RSpec.describe "ログイン", type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+  end
+  context 'ログインができる時' do
+    it '保存されているユーザーの情報と合致すればログインができる' do
+      # トップページに移動する
+      visit root_path
+      # 正しいユーザー情報を入力する
+      fill_in 'user_email', with: @user.email
+      fill_in 'user_password', with: @user.password
+      # ログインボタンを押す
+      find('input[name="commit"]').click
+      # トップページへ遷移することを確認する
+      expect(current_path).to eq root_path
+      # ログアウトボタンが表示されていることを確認する
+      expect(page).to have_content('ログアウト')
+      # サインアップへ遷移するボタンや、ログインページへ遷移するボタンが表示されていないことを確認する
+      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_content('ログイン')
+    end
+  end
+
+  context 'ログインができない時' do
+    it '保存されているユーザーの情報と合致しないとログインができない' do
+      # トップページに移動する
+      # ユーザー情報を入力する
+      # ログインボタンを押す
+      # ログインページへ戻されることを確認する
     end
   end
 end
