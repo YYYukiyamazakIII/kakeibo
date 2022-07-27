2d4d7948d7f502623bda682748ed85a40fba4949
YYYukiyamazakIII <fairy_0530_0710@yahoo.co.jp>
2022-07-27 15:27:36 +0900

    'ログインしたユーザーは自分が投稿したつぶやきの編集ができる'の実装

--- spec/system/tweets_spec.rb
+++ spec/system/tweets_spec.rb
@@ -47,3 +47,38 @@ RSpec.describe "つぶやき投稿", type: :system do
     end
   end
 end
+
+RSpec.describe 'つぶやき編集', type: :system do
+  before do
+    @tweet1 = FactoryBot.create(:tweet)
+    @tweet2 = FactoryBot.create(:tweet)
+  end
+
+  context 'つぶやき編集ができる時' do
+    it 'ログインしたユーザーは自分が投稿したつぶやきの編集ができる' do
+      # @tweet1を投稿したユーザーでログインする
+      visit root_path
+      fill_in 'user_email', with: @tweet1.user.email
+      fill_in 'user_password', with: @tweet1.user.password
+      find('input[name=commit]').click
+      # つぶやき一覧ページへ移動する
+      visit tweets_path
+      # @tweet1に編集へのリンクがあることを確認する
+      expect(
+        all('.dropdown')[0].click
+      ).to have_link '編集する', href: edit_tweet_path(@tweet1)
+      # 編集ページへ移動する
+      visit edit_tweet_path(@tweet1)
+      # 投稿内容を編集する
+      fill_in 'exampleFormControlTextarea1', with: "#{@tweet1.text}+編集済み"
+      # 編集してもTweetモデルのカウントは変わらないことを確認する
+      expect{
+        find('input[name=commit]').click
+      }.to change { Tweet.count }.by(0)
+      # 編集後はつぶやき一覧ページに移動することを確認する
+      expect(current_path).to eq tweets_path
+      # つぶやき一覧ページには先ほど変更した内容のつぶやきが存在することを確認する
+      expect(page).to have_content "#{@tweet1.text}+編集済み"
+    end
+  end
+end
